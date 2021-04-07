54f2bc5420edf0da7c8cee80038a70b51030fcfb
Сахао Тао <taosahao25@gmail.com>
2021-04-07 21:32:01 +0300

    Implemented vector class

---
+++ Vectors/Vectors/Vector.h
@@ -0,0 +1,19 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#pragma once
+#include <iostream>
+#include "Point.h"
+
+class Vector
+{
+public:
+	double n1;
+	double n2;
+	double n3;
+	Point A;
+	Point B;
+    Vector(double X, double Y, double Z);
+	Vector(Point x, Point y); 
+};
+
+#endif 
