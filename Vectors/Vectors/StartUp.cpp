93d15f9db1ffbb04a1e4723927cf51a49a2c47c1
Iggy <ineshovski@abv.bg>
2021-04-14 20:37:34 +0300

    Revert "Completed task 2.1 and wrote a test segment in the startup"
    This reverts commit d35dc01fe19016c08649f7b75d7a5a85bcd7c7cd.

--- Vectors/Vectors/StartUp.cpp
+++ Vectors/Vectors/StartUp.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include "Point.h"
-#include "Vector.h"
-#include <math.h> //testing reasons
 
 using namespace std;
 
 int main()
 {
-	//ÒÅÑÒÎÂÅ ÍÀ ÇÀÄÀ×À 2.1:
-	//Vector test(2, 2, 4);
-	////2^2 + 2^2 + 4^2 = 4 + 4 + 16 = 24; sqrt(24) = sqrt(4*6) = 2sqrt(6);
-	//double result = test.CalculateLength(test.n1, test.n2, test.n3);
-	//cout << result << endl;
-	//cout << sqrt(24);
+	//Empty for now
 }
--- Vectors/Vectors/Vector.h
+++ Vectors/Vectors/Vector.h
@@ -13,8 +13,7 @@ public:
 	Point A;
 	Point B;
     Vector(double X, double Y, double Z);
-	Vector(Point x, Point y);
-	double CalculateLength(double X, double Y, double Z); //Задача 2.1
+	Vector(Point x, Point y); 
 };
 
 #endif 
