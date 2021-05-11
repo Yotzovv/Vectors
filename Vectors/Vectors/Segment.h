badb2c81a20d4a4430fc8969f2456805820ea61e
ivyo <ivyo@WIN-0OVOR0CIS69>
2021-05-12 02:06:33 +0300

    Implemented function Find segment lngth

--- Vectors/Vectors/Segment.h
+++ Vectors/Vectors/Segment.h
@@ -9,11 +9,18 @@
 
 class Segment : public Line
 {
-public:
+private:
 	Point Start_pt;
 	Point End_pt;
+
+public:
+	void set_startPt(Point pt);
+	void set_endPt(Point pt);
+	Point get_startPt();
+	Point get_endPt();
 	Segment();
 	Segment(Point start_pt, Point end_pt);
+	double find_segment_length();
 };
 
 #endif
