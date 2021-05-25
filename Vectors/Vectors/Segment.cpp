5ba855c5a2437e13b2c6300a530c0b9b473aa7d7
I.Neshovski <ineshovski@abv.bg>
2021-05-25 23:17:30 +0300

    Implemented a fix for the function not working in 3D

--- Vectors/Vectors/Segment.cpp
+++ Vectors/Vectors/Segment.cpp
@@ -34,5 +34,5 @@ Segment::Segment(Point start_pt, Point end_pt)
 
 double Segment::find_segment_length()
 {
-	return (double)sqrt(pow(get_endPt().X - get_startPt().X, 2) + pow(get_endPt().Y - get_startPt().Y, 2));
+	return (double)sqrt(pow(get_endPt().X - get_startPt().X, 2) + pow(get_endPt().Y - get_startPt().Y, 2) + pow(get_endPt().Z - get_startPt().Z, 2));
 }
