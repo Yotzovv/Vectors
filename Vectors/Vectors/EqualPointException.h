9a35d43a4355375ea3c791598bbdda48c3fa5d8e
Кристиян Стоянов <kristiyanofthenorth@gmail.com>
2021-05-29 22:46:01 +0300

    VEC-66/ Put exceptions in seperate files

---
+++ Vectors/Vectors/EqualPointException.h
@@ -0,0 +1,29 @@
+#ifndef EQUALPOINTEXCEPTION_H
+#define EQUALPOINTEXCEPTION_H
+#pragma once
+#include <exception>
+#include <string>
+
+
+class EqualPointException : public std::exception {
+private:
+	std::string message_;
+public:
+	EqualPointException(const std::string& message);
+	const char* what() const noexcept override;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
